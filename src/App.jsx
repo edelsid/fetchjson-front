bb7810f623f15412e55cee24cf0f4009267cf4ff
Xenia Ch <eradonna@yandex.ru>
2024-01-23 16:53:29 +0300

    pathing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Data } from './components/data'
+import { Data } from './components/Data'
 import { Error } from './components/Error'
 import { Loading } from './components/Loading'
 
